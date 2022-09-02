1af28660842ac47e19b704211b92c188f1ab57b5
hitallof <hitalloufpe@gmail.com>
2022-09-02 10:50:24 -0300

    crip config

--- crip-senha/src/main/java/br/com/cripsenha/controller/UsuarioController.java
+++ crip-senha/src/main/java/br/com/cripsenha/controller/UsuarioController.java
@@ -3,16 +3,20 @@ package br.com.cripsenha.controller;
 import br.com.cripsenha.model.UsuarioModel;
 import br.com.cripsenha.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/usuario")
 public class UsuarioController {
 
-
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Autowired
     private UsuarioRepository usuarioRepository;
 
@@ -23,7 +27,24 @@ public class UsuarioController {
 
     @PostMapping("/salvar")
     public ResponseEntity<UsuarioModel> save(@RequestBody UsuarioModel usuario){
+        usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
         return ResponseEntity.ok(usuarioRepository.save(usuario));
+    }
+
+    @GetMapping("/validarsenha")
+    public ResponseEntity<Boolean> validarSenha(@RequestParam String login,@RequestParam String senha){
+
+
+        Optional<UsuarioModel> optUsuario = this.usuarioRepository.findByLogin(login);
+        if(optUsuario.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+        }
+
+        boolean valid  = passwordEncoder.matches(senha,optUsuario.get().getSenha());
+
+        HttpStatus status = (valid) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+
+            return ResponseEntity.status(status).body(valid);
     }
 
 }
